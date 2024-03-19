6187e9551696cf3de0e81497e4ddb0f1ccfa44a2
kimsoo0119 <illppang@naver.com>
2024-03-20 00:05:19 +0900

    Incomplete(#5): 검색 기능 구현중

--- src/api/search/controllers/search.controller.ts
+++ src/api/search/controllers/search.controller.ts
@@ -1,6 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { SearchService } from '../services/search.service';
 
 @Controller('search')
 export class SearchController {
-  constructor() {}
+  constructor(private readonly searchService: SearchService) {}
+
+  @Get('places')
+  async searchPlaces(@Query('value') value: string) {
+    await this.searchService.searchPlaces(value);
+  }
 }
--- src/api/search/services/search.service.ts
+++ src/api/search/services/search.service.ts
@@ -1,6 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { SearchRepository } from '../repository/search.repository';
+import axios, { AxiosResponse } from 'axios';
 
 @Injectable()
 export class SearchService {
-  constructor() {}
+  constructor(private readonly searchRepository: SearchRepository) {}
+
+  async searchPlaces(value) {
+    const response = await axios.get(
+      `https://dapi.kakao.com/v2/local/search/keyword`,
+      {
+        headers: {
+          Authorization: `KakaoAK `,
+          'Content-type': 'application/x-www-form-urlencoded',
+        },
+        params: {
+          query: value,
+        },
+      },
+    );
+    console.log(response.data.documents);
+  }
 }
