15fac7b9ac73eb1da57a0bd34c6ed91b20360925
kimsoo0119 <illppang@naver.com>
2024-05-14 23:37:29 +0900

    Docs(#67): 스크립트 수정

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cd /home/ubuntu/backend
+source /home/ubuntu/backend/.env
 
 npm ci
 
@@ -14,4 +15,4 @@ docker rm -f $(docker ps -qa)
 
 docker build -t ${DOCKER_IMAGE_NAME} . # <--- 프로젝트 루트 경로에 대한 상대경로
 
-docker run -d -p 80:8080 --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
+docker run -d -p ${PORT}:${PORT} --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
