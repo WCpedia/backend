49426bb4d86c9b7933c369326685d5e5c9996d5d
kimsoo0119 <illppang@naver.com>
2024-05-15 22:51:18 +0900

    AppSpec S3 env 테스트

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -7,14 +7,12 @@ echo ${S3_URL}
 
 aws s3 cp ${S3_URL} /home/ubuntu/tori-backend/.env
 
-npm -v
-
 npm ci
 
-# docker system prune -a -f
+docker system prune -a -f
 
-# docker rm -f $(docker ps -qa)
+docker rm -f $(docker ps -qa)
 
-# docker build -t ${DOCKER_IMAGE_NAME} . # <--- 프로젝트 루트 경로에 대한 상대경로
+docker build -t ${DOCKER_IMAGE_NAME} . # <--- 프로젝트 루트 경로에 대한 상대경로
 
-# docker run -d -p ${PORT}:${PORT} --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
+docker run -d -p ${PORT}:${PORT} --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
