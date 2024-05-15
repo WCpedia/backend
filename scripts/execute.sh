c5e33f6c3268bee0160a789765baf7925c6dc730
kimsoo0119 <illppang@naver.com>
2024-05-15 23:00:25 +0900

    Ci/Cd 테스트

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -9,10 +9,14 @@ aws s3 cp ${S3_URL} /home/ubuntu/tori-backend/.env
 
 npm ci
 
-docker system prune -a -f
+DOCKER_IMAGE_NAME=server-img
 
-docker rm -f $(docker ps -qa)
+DOCKER_CONTAINER_NAME=server-container
 
-docker build -t ${DOCKER_IMAGE_NAME} . # <--- 프로젝트 루트 경로에 대한 상대경로
+# docker system prune -a -f
 
-docker run -d -p ${PORT}:${PORT} --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
+# docker rm -f $(docker ps -qa)
+
+# docker build -t server-img . # <--- 프로젝트 루트 경로에 대한 상대경로
+
+# docker run -d -p 16243:16243 --name server-container server-img
