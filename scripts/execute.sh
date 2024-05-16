c4a9a5a4c632042183f668a1e54d9d0eaf668c45
Kimsoo0119 <96464209+Kimsoo0119@users.noreply.github.com>
2024-05-16 16:15:55 +0900

    도커 포트 재변경

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -15,4 +15,4 @@ docker rm -f $(docker ps -qa)
 
 docker build -t ${DOCKER_IMAGE_NAME} . # <--- 프로젝트 루트 경로에 대한 상대경로
 
-docker run -d -p 80:${PORT} --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
+docker run -d -p ${PORT}:${PORT} --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
