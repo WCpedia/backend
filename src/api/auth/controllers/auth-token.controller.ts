b504c0e982c80a099a8b3cf7f07cfb0c399ad20d
kimsoo0119 <illppang@naver.com>
2024-05-28 17:36:28 +0900

    Refactor: 쿠키 옵션 추가

--- src/api/auth/controllers/auth-token.controller.ts
+++ src/api/auth/controllers/auth-token.controller.ts
@@ -47,9 +47,11 @@ export class AuthTokenController {
 
     response.cookie('accessToken', token.accessToken, {
       httpOnly: true,
+      sameSite: 'none',
     });
     response.cookie('refreshToken', token.refreshToken, {
       httpOnly: true,
+      sameSite: 'none',
     });
   }
 
