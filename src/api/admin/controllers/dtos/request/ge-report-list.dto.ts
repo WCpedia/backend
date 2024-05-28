09bddb4ec267dac271afae0881c15cf4a935f4b6
kimsoo0119 <illppang@naver.com>
2024-05-28 15:59:25 +0900

    Refactor(#74): 조회 방식 무한스크롤 -> 페이지네이션으로 변경, 제공되는 데이터 중 화장실 정보 추가

--- src/api/admin/controllers/dtos/request/ge-report-list.dto.ts
+++ src/api/admin/controllers/dtos/request/ge-report-list.dto.ts
@@ -1,9 +1,45 @@
 import { PaginationDto } from '@api/common/dto/pagination.dto';
 import { ToBoolean } from '@api/common/decorators/to-boolean.decorator';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsBooleanString, IsOptional } from 'class-validator';
+import {
+  IsBoolean,
+  IsBooleanString,
+  IsNumber,
+  IsOptional,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class GetFacilityReportListDto extends PaginationDto {
+  @ApiProperty({
+    description: '현재 페이지/첫 요청 시 0 or undefined',
+    type: Number,
+    required: false,
+  })
+  @IsNumber()
+  @Type(() => Number)
+  @IsOptional()
+  currentPage: number;
+
+  @ApiProperty({
+    description: '이동할 페이지/첫 요청 시 0  or undefined',
+    type: Number,
+    required: false,
+  })
+  @IsNumber()
+  @Type(() => Number)
+  @IsOptional()
+  targetPage: number;
+
+  @ApiProperty({
+    description: '반환된 내역의 첫번째 id/첫 요청 시 0  or undefined',
+    type: Number,
+    required: false,
+  })
+  @IsNumber()
+  @Type(() => Number)
+  @IsOptional()
+  firstItemId: number;
+
   @ApiProperty({
     type: Boolean,
     description: '운영자 확인 여부',
