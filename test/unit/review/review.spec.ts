80e26813b05921bb286646bccd3c043f85f410c6
kimsoo0119 <illppang@naver.com>
2024-06-14 22:23:31 +0900

    Feat(#83): 리뷰 이미지 개수 초과 테스트 코드 추가 완료

--- test/unit/review/review.spec.ts
+++ test/unit/review/review.spec.ts
@@ -1,5 +1,7 @@
 import { Review } from '@api/review/review';
 import { CustomException } from '@exceptions/http/custom.exception';
+import { HttpExceptionStatusCode } from '@exceptions/http/enums/http-exception-enum';
+import { ReviewExceptionEnum } from '@exceptions/http/enums/review.exception.enum';
 import { ReviewImage, VisitTime } from '@prisma/client';
 import { setS3BaseUrl } from '@src/utils/s3-url-transformer';
 
@@ -7,7 +9,7 @@ describe('Review', () => {
   let reviewData;
   let s3Url: string = process.env.S3_BUCKET_URL;
 
-  beforeEach(() => {
+  beforeAll(() => {
     setS3BaseUrl(s3Url);
     reviewData = {
       id: 1,
@@ -36,11 +38,7 @@ describe('Review', () => {
 
       const updatedReview = Review.update(review, newReviewData);
 
-      expect(updatedReview.rating.accessibilityRating).toBe(4);
-      expect(updatedReview.rating.facilityRating).toBe(3);
-      expect(updatedReview.rating.cleanlinessRating).toBe(2);
-      expect(updatedReview.updateData.visitTime).toBe(VisitTime.AFTERNOON);
-      expect(updatedReview.updateData.description).toBe('Updated description');
+      expect(updatedReview.updateData).toEqual(newReviewData);
     });
   });
 
@@ -48,7 +46,7 @@ describe('Review', () => {
     it.each([
       {
         description:
-          '새 이미지 파일이 추가되고, 기존 이미지 중 일부가 삭제되는 경우',
+          '새 이미지 파일이 추가되고, 기존 이미지 중 일부가 삭제되는 경우 성공한다',
         newImageUrls: [`${s3Url}/test/oldKey1`, `${s3Url}/test/oldKey2`],
         newImageFiles: [
           { key: 'test/fileKey1' } as Express.MulterS3.File,
@@ -64,7 +62,7 @@ describe('Review', () => {
       },
       {
         description:
-          '새 이미지 파일만 추가되고, 모든 기존 이미지가 삭제되는 경우',
+          '새 이미지 파일만 추가되고, 모든 기존 이미지가 삭제되는 경우 성공한다',
         newImageUrls: [],
         newImageFiles: [
           { key: 'test/fileKey1' } as Express.MulterS3.File,
@@ -79,7 +77,8 @@ describe('Review', () => {
         expectedImagesToDelete: [1, 2, 3],
       },
       {
-        description: '새 이미지 파일이 없고, 모든 기존 이미지가 삭제되는 경우',
+        description:
+          '새 이미지 파일이 없고, 모든 기존 이미지가 삭제되는 경우 성공한다',
         newImageUrls: [],
         newImageFiles: [],
         currentImages: [
@@ -107,6 +106,65 @@ describe('Review', () => {
 
         expect(imagesToAdd).toEqual(expectedImagesToAdd);
         expect(imagesToDelete).toEqual(expectedImagesToDelete);
+      },
+    );
+
+    it.each([
+      {
+        description: '기존0 추가6 = 6',
+        newImageUrls: [],
+        newImageFiles: [
+          { key: 'test/fileKey1' } as Express.MulterS3.File,
+          { key: 'test/fileKey2' } as Express.MulterS3.File,
+          { key: 'test/fileKey3' } as Express.MulterS3.File,
+          { key: 'test/fileKey4' } as Express.MulterS3.File,
+          { key: 'test/fileKey5' } as Express.MulterS3.File,
+          { key: 'test/fileKey6' } as Express.MulterS3.File,
+        ],
+        currentImages: [] as ReviewImage[],
+      },
+      {
+        description: '기존2 추가4 = 6',
+        newImageUrls: [`${s3Url}/test/oldKey1`, `${s3Url}/test/oldKey2`],
+        newImageFiles: [
+          { key: 'test/fileKey1' } as Express.MulterS3.File,
+          { key: 'test/fileKey2' } as Express.MulterS3.File,
+          { key: 'test/fileKey3' } as Express.MulterS3.File,
+          { key: 'test/fileKey4' } as Express.MulterS3.File,
+        ],
+        currentImages: [
+          { id: 1, key: 'test/oldKey1' },
+          { id: 2, key: 'test/oldKey2' },
+        ] as ReviewImage[],
+      },
+
+      {
+        description: '삭제2 추가4 기존4 = 6',
+        newImageUrls: [`${s3Url}/test/oldKey1`, `${s3Url}/test/oldKey2`],
+        newImageFiles: [
+          { key: 'test/fileKey1' } as Express.MulterS3.File,
+          { key: 'test/fileKey2' } as Express.MulterS3.File,
+          { key: 'test/fileKey3' } as Express.MulterS3.File,
+          { key: 'test/fileKey4' } as Express.MulterS3.File,
+        ],
+        currentImages: [
+          { id: 1, key: 'test/oldKey1' },
+          { id: 2, key: 'test/oldKey2' },
+          { id: 3, key: 'test/oldKey3' },
+          { id: 4, key: 'test/oldKey4' },
+        ] as ReviewImage[],
+      },
+    ])(
+      '이미지가 지정한 이미지 수를 초과하면 Exception이 발생 한다. $description',
+      ({ newImageUrls, newImageFiles, currentImages }) => {
+        const error = new CustomException(
+          HttpExceptionStatusCode.BAD_REQUEST,
+          ReviewExceptionEnum.REVIEW_IMAGE_LIMIT_EXCEEDED,
+        );
+
+        expect(() =>
+          Review.updateImages(newImageUrls, newImageFiles, currentImages),
+        ).toThrow(error);
       },
     );
   });
