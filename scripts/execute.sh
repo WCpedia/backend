c19e35bc773d27922c3eb72fb301555004d1d90f
kimsoo0119 <illppang@naver.com>
2024-08-05 16:29:18 +0900

    Feat(#130): 서버 알림 구현 완료

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -3,7 +3,7 @@
 cd /home/ubuntu/tori-backend
 source /etc/profile
 
-echo ${S3_URL}
+./scripts/deploy-start.sh
 
 aws s3 cp ${S3_URL} /home/ubuntu/tori-backend/.env
 
@@ -16,3 +16,5 @@ docker rm -f $(docker ps -qa)
 docker build -t ${DOCKER_IMAGE_NAME} . # <--- 프로젝트 루트 경로에 대한 상대경로
 
 docker run -d -p ${PORT}:${PORT} --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
+
+./scripts/server-on.sh
